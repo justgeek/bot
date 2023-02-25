a3e01ae2e62497d26cc8911fed2dc5b8e54efc11
Amr Hisham <darkstarlet@gmail.com>
2023-02-25 23:19:57 +0200

    joke

--- bot.js
+++ bot.js
@@ -186,12 +186,12 @@ client.on("messageCreate", (msg) => {
   if (msg.author.username + "#" + msg.author.discriminator == "Malevolent#0025") return;
 
   // console.log(msg.guild.emojis.cache)//show all emojis
-  // if (msg.author.username + "#" + msg.author.discriminator == "exorcismus#7611" && !message.startsWith("!")) {
+  if (msg.author.username + "#" + msg.author.discriminator == "exorcismus#7611" && !message.startsWith("!")) {
     // if (msg.author.username + '#' + msg.author.discriminator == 'Moonscarlet#4105') {
     // msg.react(msg.guild.emojis.cache.get('515873f6898e0b26daf51921c65a43f7'))//BRUH
     // msg.react(':regional_indicator_a:')
-    // msg.react(msg.guild.emojis.cache.get("1018204796689322014")); //BRUH
-  // }
+    msg.react(msg.guild.emojis.cache.get("1018204796689322014")); //BRUH
+  }
 
   if (message === "!commands") {
     // msg.delete();
@@ -433,12 +433,14 @@ client.on("presenceUpdate", (before, after) => {
   ) {
     // sendToChannel(IDs.channelStatus, 'E2FESH <@' + IDs.Moonscarlet + '> CHC ¿¿¿? xdDDD¡¡¡');
     // sendToChannel(IDs.channelStatus, 'Welcome back <@' + IDs.Moonscarlet + '>');
+    // sendToChannel(IDs.channelStatus, '<@' + IDs.Moonscarlet + '> RIP in pieces Lady Demashkash 🦇');
   } else if (
-    userID == IDs.LORD &&(currentHour >= 18 || currentHour <= 2) &&
+    userID == IDs.LORD &&(currentHour >= 17 || currentHour <= 2) &&
     (statusBefore == "offline" || statusBefore == " ") && (statusAfter == "online" || statusAfter == "idle")
   ) {
     // sendToChannel(IDs.channelMain, 'E2FESH <@' + IDs.LORD + '> CHC ¿¿¿? xdDDD¡!¡!¡');
     // const chatMsg ="<@" + IDs.LORD + "> \n1-7etta fel tricks?\nOR\n2-7etta fel trade?";
+    // const chatMsg ="<@" + IDs.LORD + "> 🦇🧛‍♀️🦇 RIP IN PIECES LADY DEMETGHASHKAR 🦇🧛‍♀️🦇";
     // const chatMsg = "<@" + IDs.LORD + "> Lady Dimitrescu in the village is waiting for you (approximately 290 centimeters tall in her heels and fabulous hat)";
     // sendToChannel(IDs.channelMain, chatMsg);
   }
