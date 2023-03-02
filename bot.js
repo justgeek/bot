5c14a8b55f40ec32249c79dcd07841816dbdad93
mohamedtaher <mt@prollygeek.com>
2023-03-02 03:11:28 +0100

    add webserver

--- bot.js
+++ bot.js
@@ -578,3 +578,15 @@ function getCurrentWindow() {
 }
 
 client.login(process.env.BOT_TOKEN);
+
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
